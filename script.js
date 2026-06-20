1ec0ab7099164d0425b9d6662d341592520948ec
olli-111-afk <olli-111@gmx.de>
2026-06-20 15:07:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const button = document.getElementById("startButton");
+
+    button.addEventListener("click", () => {
+
+        alert("Willkommen! 😊\n\nAls Nächstes bauen wir hier das Anmeldeformular.");
+
+    });
+
+});
